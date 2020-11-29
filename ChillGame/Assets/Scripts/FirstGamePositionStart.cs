357e754c5b404610596fcbc70e3684cb7b87820d
iletai <iletai@hotmail.com>
2020-11-29 23:11:38 +0700

    + Init project

---
+++ ChillGame/Assets/Scripts/FirstGamePositionStart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstGamePositionStart : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Current state: " + GameManager.Instance.gameState);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void NewGameOnClicked()
+    {
+        GameManager.Instance.SetStateChange(GAMESTATE.MAIN_MENU);
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGameOnClicked()
+    {
+        GameManager.Instance.SetStateChange(GAMESTATE.EXIT);
+        Application.Quit();
+    }    
+}
---
+++ ChillGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GAMESTATE
+{
+    LOADING,
+    GAMEPLAY,
+    PAUSE,
+    EXIT,
+    MAIN_MENU,
+    NONE
+}
+
+public delegate void OnSateChangeHandler();
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance = null;
+    public event OnSateChangeHandler onGameStateChange;
+    public GAMESTATE gameState {
+        get;
+        set;
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        _instance = this;
+    }
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameManager();
+            }
+            return _instance;
+        }
+    }
+
+    public void SetStateChange(GAMESTATE gamestate)
+    {
+        gameState = gamestate;
+        Debug.Log("Set state: " + gameState);
+    }
+
+    public void OnApplicationQuit()
+    {
+        _instance = null;
+    }
+
+}
